c5178b2ce31b55794b16a9a52f7e6b3462b4cc9b
Roni Akiola <roni.akiola@hotmail.com>
2023-05-06 14:46:24 +0300

    testing

--- src/views/Brewery.tsx
+++ src/views/Brewery.tsx
@@ -3,10 +3,16 @@ import useBreweries from '../hooks/useBreweries';
 
 const Brewery = () => {
   const { id } = useParams();
-  // const { breweries, loading } = useBreweries();
+  const { breweries, loading } = useBreweries(id);
+  const singleBrewery = breweries[0];
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      <h1>Brewery details</h1>
+      <h1>{singleBrewery.name}</h1>
     </>
   );
 };
